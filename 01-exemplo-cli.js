4e47fa7449a934fcc44f83f3f6b596a1dc84b0c9
heloisa0804 <heloisa.germanobussi@yahoo.com.br>
2025-06-05 11:46:55 -0300

    uso da cli, escopo de variáveis constantes

---
+++ 01-exemplo-cli.js
@@ -0,0 +1,14 @@
+//Habilitando o modo escrito de linguagem
+'use strict';
+
+
+let preco = 100;
+let resultado;
+
+if(preco >=50){
+    resultado = "Beleza";
+}else {
+    resultado = "Que droga";
+}
+
+console.log(resultado);
