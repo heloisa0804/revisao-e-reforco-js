b9f1c29f6535103e330c559dcbd6c5520c594424
heloisa0804 <heloisa.germanobussi@yahoo.com.br>
2025-06-06 08:50:03 -0300

    usando operador +=

--- 02-lidando-com-strings.js
+++ 02-lidando-com-strings.js
@@ -9,9 +9,16 @@ let estado = "SP";
 // CONCATENAÇÃO tradicional é Chapolin Colorado, tenho 20 anos e moro em São Paulo/Sp.
 
 //CONCATENAÇÃO tradicional usa operador +
-let mensagem1 = "Meu nome é "+nome+" "+sobrenome+", tenho "+idade+" anos e moro em "+cidade+"/"+estado+".";
+//let mensagem1 = "Meu nome é "+nome+" "+sobrenome+", tenho "+idade+" anos e moro em "+cidade+"/"+estado+".";
 
-console.logI(mensagem1);
+//Usando operador += para concatenar "por pedaços/partes"
+let mensagem1 = "Meu nome é "+nome;
+mensagem1 += ""+sobrenome+",tenho "+idade;
+mensagem1 += "anos e moro em"+cidade;
+mensagem1 += "/"+estado+".";
+
+
+console.log(mensagem1);
 
 
 
