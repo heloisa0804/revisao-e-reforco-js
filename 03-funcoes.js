8cc24c9d448f9cce286673ffc112e9ca9873856d
heloisa0804 <heloisa.germanobussi@yahoo.com.br>
2025-06-06 10:37:11 -0300

    adiciona exercício

--- 03-funcoes.js
+++ 03-funcoes.js
@@ -35,5 +35,20 @@ exemplo2(); // chamada (sempre após declarar a função anônima)
 
 separador();
 
-/* */
+/* 1) Crie uma função (qualquer sintaxe) que receba dois valores numericos como parâmetros, calcule a diferença entre valores e retorne o resultado calculado */
 
+/* 2) Chame esta função pelo menos duas vezes passando valores diferentes e guardando os resultados em variáveis de escopo global. */
+
+/* 3) Apresente os resultados no console. */
+
+
+function calculoDiferenca(a,b) {
+    let resultado = a - b;
+   return resultado ; 
+}
+
+let resultado1 = calculoDiferenca(20,10);
+let resultado2 = calculoDiferenca (30,10);
+
+console.log("Resultado 1:", resultado1); 
+console.log("Resultado 2:", resultado2); 
