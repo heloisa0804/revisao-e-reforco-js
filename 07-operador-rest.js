114af9198ae692b67930f196e6e412cc7b801efa
heloisa0804 <heloisa.germanobussi@yahoo.com.br>
2025-06-10 11:00:47 -0300

    adiciona variavel acumuladora e interação somar e guardar total

--- 07-operador-rest.js
+++ 07-operador-rest.js
@@ -1,3 +1,21 @@
 'use strict';
 
+/* Sobre o operador rest...
+Usamos o rest para mesclar uma relação de parâmetros/argumentos para uma função. Na prática, os parâmetros se comportam como um array podendo receber iterações via loop. */
+ 
+function somar(...valores){
+    //Variável acumuladora
+    let total = 0;
 
+    //Interação para somar e guardar o total
+    for(const valor of valores){
+        total += valor;
+        
+    }
+    return total;
+}
+
+let resultado1 = somar(10,20);
+let resultado2 = somar(10,20,50);
+console.log(resultado1);
+console.log(resultado2);
