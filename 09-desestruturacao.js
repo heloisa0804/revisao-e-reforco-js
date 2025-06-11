a4dab63443767e6a461ad0c41c63baa92b5725a0
heloisa0804 <heloisa.germanobussi@yahoo.com.br>
2025-06-11 08:47:19 -0300

    Desestrutura parcialmente valores

--- 09-desestruturacao.js
+++ 09-desestruturacao.js
@@ -10,3 +10,20 @@ const [cliente1, cliente2, cliente3] = clientes;
 console.log(cliente1);
 console.log(cliente2);
 console.log(cliente3);
+
+separador();
+
+const capitais = [
+  "João Pessoa",
+  "Manaus",
+  "Curitiba",
+  "Salvador",
+  "Belo Horizonte",
+];
+
+// Desestruturando alguns valores
+const [paraiba, amazonas, , bahia] = capitais;
+
+console.log(paraiba);
+console.log(amazonas);
+console.log(bahia);
