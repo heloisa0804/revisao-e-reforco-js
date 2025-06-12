4d6bc2285577f09f276f2f4fd8fa755627dfa715
heloisa0804 <heloisa.germanobussi@yahoo.com.br>
2025-06-12 11:15:43 -0300

    exercício feito

--- 10-map.js
+++ 10-map.js
@@ -46,6 +46,15 @@ console.log("======================");
 
 separador();
 
-//Exemplo 2: gerar um novo array contendo somente o s títulos dos cursos
+//Exemplo 2: gerar um novo array contendo somente os títulos dos cursos
 const titulos = cursos.map((curso) => curso.titulo);
 console.log(titulos);
+
+/* Exercícios
+Use o map para gerar um novo array contendo SOMENTE os preços dos cursos calculados com  um desconto de 10%.
+*/
+
+const precosComDesconto = cursos.map(
+  (curso) => curso.preco - curso.preco * 0.1
+);
+console.log(precosComDesconto);
